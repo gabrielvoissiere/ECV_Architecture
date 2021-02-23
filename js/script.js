e102a1f3b9e1ae58d9f74e4beda702dfa3b6c83f
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2021-02-23 16:27:51 +0100

    fin

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var burger = document.getElementById("burger");
+
+burger.addEventListener("click", function() {
+    document.getElementById("nav").classList.toggle("slide")
+    document.getElementById("l1").classList.toggle("croix-l1")
+    document.getElementById("l2").classList.toggle("croix-l2")
+    document.getElementById("l3").classList.toggle("croix-l3")
+})
